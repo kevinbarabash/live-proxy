52fc233a77d45e592afafd2f26997820b133ea9f
Kevin Barabash <kevinb7@gmail.com>
2016-03-08 09:02:43 -0800

    more descriptive naming of variables

--- src/index.js
+++ src/index.js
@@ -14,11 +14,9 @@ const p = new Processing(canvas, (processing) => {
 });
 
 
-// env stores objects between code changes
-// TODO: rename this to context
-const env = {};
+// Persists objects between code changes and re-runs of the code.
+const persistentContext = {};
 
-// archive stores hashes of stringified objects
 // The key is the object's identifier.  The object is stringified right (and
 // hashed) after the main body of the user code is run.  If the hashes match
 // then that means that the main body didn't do anything different when creating
@@ -26,7 +24,7 @@ const env = {};
 // that's inside env.  If not, we replace the object in env with the newly
 // initialized object for that identifier.
 // TODO: figure out how to save space if multiple identifiers point to the same object
-const archive = {};
+const objectHashes = {};
 
 
 
@@ -52,16 +50,16 @@ const pGlobals = "/*global " +
     "*/\n";
 
 
-const injectFunctions = function(newEnv, funcList) {
-    Object.keys(archive).forEach(name => {
-        const value = archive[name];
+const injectFunctions = function(newContext, funcList) {
+    Object.keys(persistentContext).forEach(name => {
+        const value = persistentContext[name];
 
         if (typeof value === 'function') {
             Object.keys(value.prototype).forEach(key => {
                 delete value.prototype[key];
             });
 
-            Object.defineProperty(newEnv, name, {
+            Object.defineProperty(newContext, name, {
                 enumerable: true,
                 get() {
                     return funcList[name] ? value : undefined;
@@ -71,7 +69,7 @@ const injectFunctions = function(newEnv, funcList) {
                         // TODO: re-run the whole thing
                         // if there are no objects created by calling the
                         // constructor then we don't have to rerun anything
-                        archive[name] = newValue;
+                        persistentContext[name] = newValue;
                     }
                     funcList[name] = true;
                 }
@@ -81,33 +79,36 @@ const injectFunctions = function(newEnv, funcList) {
 };
 
 
-const updateEnvironments = function(env, newEnv, funcList) {
-    Object.keys(newEnv).forEach(name => {
-        const value = newEnv[name];
+const updateEnvironments = function(persistentContext, newContext, funcList) {
+    Object.keys(newContext).forEach(name => {
+        const value = newContext[name];
 
         if (typeof value === 'object') {
             const hash = md5(JSON.stringify(value));
 
-            // even though we don't do anything with newEnv directly,
-            // the objects it stores can be updated via callbacks and
-            // event handlers that are running in between updates to the
-            // code
-            if (archive[name] === hash) {
-                newEnv[name] = env[name];
+            // Even though we don't do modify newContext directly, the objects
+            // it stores can be updated via callbacks and event handlers that
+            // are running in between updates to the code.  The objects in
+            // persistentContext will contain any changes to those objects.  As
+            // long as the hashes match it's safe to replace the object in the
+            // new context with the one that's been accumulating changes from
+            // the persisten context.
+            if (objectHashes[name] === hash) {
+                newContext[name] = persistentContext[name];
             } else {
-                env[name] = newEnv[name];
-                archive[name] = hash;
+                persistentContext[name] = newContext[name];
+                objectHashes[name] = hash;
             }
         } else if (typeof value === 'function') {
-            if (archive.hasOwnProperty(name)) {
-                // if the function is in the archive but we didn't
+            if (persistentContext.hasOwnProperty(name)) {
+                // if the function is in the persistent context but we didn't
                 // define the last time the code changed delete it
                 if (!funcList[name]) {
-                    delete archive[name];
+                    delete persistentContext[name];
                 }
             } else {
-                // if the function isn't in the archive add it
-                archive[name] = value;
+                // if the function isn't in the persitent context add it
+                persistentContext[name] = value;
             }
         }
     });
@@ -133,14 +134,14 @@ const handleUpdate = function() {
             window.transformedCode = transformedCode;
 
             const func = new Function('__env__', 'customWindow', 'p', transformedCode);
-            const newEnv = {};
+            const newContext = {};
             const funcList = {};    // functions being defined during this run
 
-            injectFunctions(newEnv, funcList);
+            injectFunctions(newContext, funcList);
 
-            func(newEnv, customWindow.window, p);
+            func(newContext, customWindow.window, p);
 
-            updateEnvironments(env, newEnv, funcList);
+            updateEnvironments(persistentContext, newContext, funcList);
 
             canvas.style.opacity = 1.0;
         } catch(e) {
